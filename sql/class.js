5f1e4f1b98c13aa7313804ff2ce0d7532dcff0c0
KasuganoSora1 <zhang192831@gmail.com>
2023-06-06 18:04:23 +0800

    继续输入

---
+++ sql/class.js
@@ -0,0 +1,14 @@
+function borehole(){
+    this.project_index="";
+    this.borhole_index="";
+    this.X=0.0;
+    this.Y=0.0;
+    this.hei=0.0;
+    this.depth=0.0;
+    this.lithologic="";
+    this.project_name="";
+    this.dis=0;
+}
+module.exports={
+    borehole:borehole
+}
